efb6ae4542693a53d1ee63327e4b071fb0495606
Leonardo Cardoso <leo@leocardz.com>
2014-05-25 02:57:15 -0300

    Playing song

--- Android Audio Preview/src/main/java/com/leocardz/android/audio/preview/MainActivity.java
+++ Android Audio Preview/src/main/java/com/leocardz/android/audio/preview/MainActivity.java
@@ -1,36 +1,221 @@
 package com.leocardz.android.audio.preview;
 
-import android.support.v7.app.ActionBarActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private final String URL = "https://dl.dropbox.com/s/hgcgq510urmgmed/01%20Main%20Title.mp3";
+
+    private View board;
+    private EditText urlEdittext;
+    private Button actionButton, pause, stop;
+
+    private LinearLayout buttons, loading;
+
+    private TextView songTitle, songArtist;
+
+    private Animation songSlideUp, songSlideDown;
+
+    private RelativeLayout.LayoutParams params;
+
+    private MediaPlayer mediaPlayer;
+
+    private boolean isAnimating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUpIds();
+
+        urlEdittext.setText(URL);
+
+        setUpListeners();
+
+        setUpMediaPlayer();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    private void setUpMediaPlayer() {
+        if (mediaPlayer == null) {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setOnBufferingUpdateListener(bufferingUpdateListener);
+            mediaPlayer.setOnCompletionListener(completionListener);
         }
-        return super.onOptionsItemSelected(item);
     }
+
+    private void setUpIds() {
+        songSlideUp = AnimationUtils.loadAnimation(this, R.anim.song_slide_up);
+        songSlideDown = AnimationUtils.loadAnimation(this, R.anim.song_slide_down);
+
+        isAnimating = false;
+
+        params = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        board = findViewById(R.id.board);
+        urlEdittext = (EditText) findViewById(R.id.url_edittext);
+        actionButton = (Button) findViewById(R.id.action_button);
+        pause = (Button) findViewById(R.id.pause);
+        stop = (Button) findViewById(R.id.stop);
+        buttons = (LinearLayout) findViewById(R.id.buttons);
+        loading = (LinearLayout) findViewById(R.id.loading);
+        songTitle = (TextView) findViewById(R.id.song_title);
+        songArtist = (TextView) findViewById(R.id.song_artist);
+    }
+
+    private void setUpListeners() {
+        actionButton.setOnClickListener(startListener);
+        pause.setOnClickListener(pauseListener);
+        stop.setOnClickListener(stopListener);
+        songSlideUp.setAnimationListener(slideUpAnimationListener);
+        songSlideDown.setAnimationListener(slideDownAnimationListener);
+    }
+
+    private Animation.AnimationListener slideUpAnimationListener = new Animation.AnimationListener() {
+        @Override
+        public void onAnimationStart(Animation animation) {
+            isAnimating = true;
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            isAnimating = false;
+            params.setMargins(0, 0, 0,  getResources().getDimensionPixelSize(R.dimen.info_height));
+            params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+            board.setLayoutParams(params);
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
+    };
+
+    private Animation.AnimationListener slideDownAnimationListener = new Animation.AnimationListener() {
+        @Override
+        public void onAnimationStart(Animation animation) {
+            isAnimating = true;
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            isAnimating = false;
+            params.setMargins(0, 0, 0, getResources().getDimensionPixelSize(R.dimen.info_height_offset));
+            params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+            board.setLayoutParams(params);
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
+    };
+
+    private void streamSong(String url) {
+        if (mediaPlayer != null && !isAnimating) {
+            try {
+                mediaPlayer.setDataSource(urlEdittext.getText().toString());
+                mediaPlayer.setOnPreparedListener(preparedListener);
+                mediaPlayer.prepareAsync();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private View.OnClickListener startListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (mediaPlayer != null && !mediaPlayer.isPlaying() && !isAnimating) {
+                songTitle.setText("Main Theme");
+                songArtist.setText("Game of Thrones");
+                loading.setVisibility(View.VISIBLE);
+                buttons.setVisibility(View.GONE);
+                streamSong(URL);
+                board.startAnimation(songSlideUp);
+            }
+        }
+    };
+
+    private View.OnClickListener pauseListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            toggleMediaPlayer();
+        }
+    };
+
+    private View.OnClickListener stopListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (mediaPlayer != null && !isAnimating) {
+                mediaPlayer.stop();
+                pause.setText(R.string.play);
+                board.startAnimation(songSlideDown);
+            }
+        }
+    };
+
+    private void toggleMediaPlayer() {
+        if (mediaPlayer != null && !isAnimating) {
+            if (!mediaPlayer.isPlaying()) {
+                mediaPlayer.start();
+                pause.setText(R.string.pause);
+            } else {
+                mediaPlayer.pause();
+                pause.setText(R.string.play);
+            }
+        }
+    }
+
+    private MediaPlayer.OnPreparedListener preparedListener = new MediaPlayer.OnPreparedListener() {
+        @Override
+        public void onPrepared(MediaPlayer mediaPlayer) {
+            if (mediaPlayer != null) {
+                mediaPlayer.start();
+            }
+        }
+    };
+
+    private MediaPlayer.OnCompletionListener completionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            mediaPlayer.stop();
+            pause.setText(R.string.pause);
+            actionButton.setEnabled(true);
+            actionButton.setText(R.string.play);
+        }
+    };
+
+    private MediaPlayer.OnBufferingUpdateListener bufferingUpdateListener = new MediaPlayer.OnBufferingUpdateListener() {
+        @Override
+        public void onBufferingUpdate(MediaPlayer mediaPlayer, int i) {
+            loading.setVisibility(View.GONE);
+            buttons.setVisibility(View.VISIBLE);
+        }
+    };
+
+    @Override
+    protected void onStop() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
+        super.onStop();
+    }
+
 }
